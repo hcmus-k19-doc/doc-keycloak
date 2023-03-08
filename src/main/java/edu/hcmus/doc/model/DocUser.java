97e19f28a6b2ecaac3ee6f386395c8204ebe5d8d
Nguyen Duc Nam <66512409+namworkmc@users.noreply.github.com>
2023-03-08 16:34:59 +0700

    [DOC-111] Change to dynamic roles instead of fixed roles (#2)

--- src/main/java/edu/hcmus/doc/model/DocUser.java
+++ src/main/java/edu/hcmus/doc/model/DocUser.java
@@ -1,6 +1,5 @@
 package edu.hcmus.doc.model;
 
-import edu.hcmus.doc.model.enums.DocRole;
 import java.util.Set;
 import lombok.Data;
 
@@ -10,5 +9,5 @@ public class DocUser {
     private String id;
     private String username;
     private String email;
-    private Set<DocRole> roles;
+    private Set<String> roles;
 }
